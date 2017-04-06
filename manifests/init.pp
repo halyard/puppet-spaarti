7344b509ee3d02b94d33c316765aba4ff324d3de
Les Aker <me@lesaker.org>
2017-04-05 23:18:19 -0400

    fix linting

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,20 +13,20 @@ class spaarti (
   ruby_gem { 'spaarti':
     gem          => 'spaarti',
     ruby_version => '*'
-  } ->
-  file { "/Users/${::user}/.octoauth.d/spaarti.yml":
+  }
+  -> file { "/Users/${::user}/.octoauth.d/spaarti.yml":
     content => template('spaarti/octoauth.yml'),
     mode    => '0600',
     require => Class['::dotfiles']
-  } ->
-  exec { 'spaarti':
+  }
+  -> exec { 'spaarti':
     command  => "sudo -u ${::user} spaarti",
     user     => 'root',
     timeout  => 0,
     schedule => 'daily',
     require  => Class['::ssh']
-  } ->
-  exec { "/Users/${::user}/.bin/repo_sync":
+  }
+  -> exec { "/Users/${::user}/.bin/repo_sync":
     cwd      => $codedir,
     timeout  => 0,
     schedule => 'daily'
